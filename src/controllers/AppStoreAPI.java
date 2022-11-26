de48c80e888dfb9a0b9598cd457d589da4bdcfcf
Jen Cullinan <113446385+jencull@users.noreply.github.com>
2022-11-26 19:18:48 +0000

    26th Nov

--- src/controllers/AppStoreAPI.java
+++ src/controllers/AppStoreAPI.java
@@ -4,6 +4,7 @@ import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.xml.DomDriver;
 import models.*;
 import utils.*;
+import utils.ISerializer;
 
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -15,39 +16,323 @@ import java.util.List;
 import static java.lang.Math.random;
 import static utils.RatingUtility.generateRandomRating;
 
-public class AppStoreAPI {
+public class AppStoreAPI implements ISerializer {
+
+    private ArrayList<App> apps;
+
+    /**
+     * Constructor to create an ArrayList to hold apps
+     */
+    public AppStoreAPI () { apps = new ArrayList<App>(); }
+
+    /**
+     * Method returns the apps in the arrayList
+     * @return object
+     */
+    public ArrayList<App> getApps() {
+        return apps;
+    }
+
+    public void setApps(ArrayList<App> apps) {
+        this.apps = apps;
+    }
+
+
+    //Basic CRUD on apps ArrayList
+    /**
+     * Method adds an app to the arrayList
+     * @param app object to be added to arrayList
+     */
+    public boolean add(App app) {
+        boolean isAdded = false;
+
+        if (isAdded) {
+            return apps.add(app);
+        }
+        else return isAdded;
+    }
+
+    /**
+     * Method to locate an app in the arrayList
+     * @param index int used to search for the index number and the object stored at that index number
+     * @return the index number, if valid. Return null if there is no object at that location.
+     */
+    public App getAppByIndex (int index) {
+        if (isValidIndex(index)) {
+            return apps.get(index);
+        }
+        return null;
+    }
+
+    public int numberOfApps() {
+        return apps.size();
+    }
+
+    /**
+     * Method to delete an app using the index number
+     * @param indexToDelete int representing the index number of the app
+     * @return the app object that has been deleted. Return null if no object deleted.
+     */
+    public App deleteAppByIndex (int indexToDelete) {
+
+        if (isValidIndex(indexToDelete)) {
+            App app = getAppByIndex(indexToDelete);
+
+                apps.remove(indexToDelete);
+                return app;
+            }
+        else {
+            return null;
+        }
+    }
+
+    /**
+     * search for an app by name
+     * @param name String representing the name of the app
+     * @return the app if exists, otherwise null
+     */
+    public App getAppByName (String name) {
+
+        if (isValidAppName(name)) {
+            App app = getAppByName(name);
+            return app;
+        }
+        else {
+            return null;
+        }
+    }
+
+    //Reporting methods
+
+    /**
+     * Method to list all the apps held in the arrayList
+     * @return String built from the app objects. If no apps present return "no apps"
+     */
+    public String listAllApps () {
+        String list = "";
+
+        for (App app : apps) {
+            list += "\n" + apps.indexOf(app) + ": " + app;
+        }
+        if (apps.isEmpty()) {
+            return "No apps";
+        }
+        else {
+            return list;
+        }
+    }
+
+    /**
+     * Method to return summary info of all apps in the arrayList
+     * @return String representing summary object info. String is 'no apps' if arrayList empty
+     */
+    public String listSummaryOfAllApps () {
+        String list = "";
+
+        for (App app : apps) {
+            list += "\n" + apps.indexOf(app) + ": " + app.appSummary();
+        }
+        if (apps.isEmpty()) {
+            return "No apps";
+        }
+        else {
+            return list;
+        }
+    }
+
+    /**
+     * Method to list all game apps held in the arrayList
+     * @return String built from the game apps. Return 'no game apps' if no game apps present
+     */
+    public String listAllGameApps () {
+        String list = "";
+
+        for (App app : apps) {
+            if (app instanceof GameApp) {
+                list += "\n" + apps.indexOf(app) + ": " + app;
+            }
+        }
+        if (list.isEmpty()) {
+            return "No game apps";
+        }
+        else {
+            return list;
+        }
+    }
+
+    /**
+     * Method to return education apps held in the arrayList
+     * @return String built from the education apps. 'No education apps' return if no apps held
+     */
+    public String listAllEducationApps () {
+        String list = "";
+
+        for (App app : apps) {
+            if (app instanceof EducationApp) {
+                list += "\n" + apps.indexOf(app) + ": " + app;
+            }
+        }
+        if (list.isEmpty()) {
+            return "No Education apps";
+        }
+        else {
+            return list;
+        }
+    }
+
+    /**
+     * Method to list all the productivity apps held in the arrayList
+     * @return String built from apps. If none present return 'no productivity apps'
+     */
+    public String listAllProductivityApps () {
+        String list = "";
+
+        for (App app : apps) {
+            if (app instanceof ProductivityApp) {
+                list += "\n" + apps.indexOf(app) + ": " + app;
+            }
+        }
+        if (list.isEmpty()) {
+            return "No Productivity apps";
+        }
+        else {
+            return list;
+        }
+    }
+
+    /**
+     * Method to return list of apps by name
+     * @param name String representing the name of the app
+     * @return String built of returned apps
+     */
+    public String listAllAppsByName (String name) {
+        String list = "";
+
+        for (App app : apps) {
+            if (isValidAppName(name)) {
+                list += "\n" + apps.indexOf(app) + ": " + app;
+            }
+        }
+        if (list.isEmpty()) {
+            return "No apps for " + name;
+        }
+        else {
+            return list;
+        }
+    }
+
+    public String listAllAppsAboveOrEqualAGivenStarRating (int rating) {
+        String list = "";
+
+       for (App app : apps) {
+               if (rating >= app.calculateRating()) {
+                   list += "\n" + apps.indexOf(app) + ": " + app;
+               }
+           }
+       if (list.isEmpty()) {
+           return "No apps have a rating of " + rating + " or above";
+       }
+       else {
+           return list;
+       }
+    }
+
+    /**
+     * Method to list all recommended apps held in the arrayList
+     * @return String built from the recommended apps, if none return 'no recommended apps'
+     */
+    public String listAllRecommendedApps () {
+        String list = "";
+
+        for (App app : apps) {
+            if (app.isRecommendedApp()) {
+                list += "\n" + apps.indexOf(app) + ": " + app;
+            }
+        }
+        if (list.isEmpty()) {
+            return "No recommended apps ";
+        }
+        else {
+            return list;
+        }
+    }
+
+    public String listAllAppsByChosenDeveloper (Developer developer) {
+        String list = "";
+
+        for (App app : apps) {
+            if (app.getDeveloper().equals(developer)) {
+                list += "\n" + apps.indexOf(app) + ": " + app;
+            }
+        }
+            if (list.isEmpty()) {
+                return "No apps for developer: " + developer;
+        }
+            else {
+                return list;
+            }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //TODO refer to the spec and add in the required methods here (make note of which methods are given to you first!)
 
-    //---------------------
-    // Method to simulate ratings (using the RatingUtility).
-    // This will be called from the Driver (see skeleton code)
-    //---------------------
-    // TODO UNCOMMENT THIS COMPLETED method as you start working through this class
-    //---------------------
-    /*
     public void simulateRatings(){
         for (App app :apps) {
             app.addRating(generateRandomRating());
         }
-    }*/
+    }
 
-    //---------------------
-    // Validation methods
-    //---------------------
-    // TODO UNCOMMENT THIS COMPlETED method as you start working through this class
-    //---------------------
-    /*
+    /**
+     * Method to check that the name of an app is valid
+     * @param appName string with the name of the app
+     * @return boolean, true if the name is valid, otherwise false
+     */
+    public boolean isValidAppName (String appName) {
+
+        boolean isValidName = false;
+
+        if (apps.isEmpty()) {
+            return isValidName;
+        }
+        else {
+            for (int i = 0; i<apps.size(); i++) {
+                if (apps.get(i).getAppName().toLowerCase().contains(appName.toLowerCase())) {
+                    return isValidName;
+                }
+            }
+        }
+        return isValidName;
+    }
+
+    /**
+     * Method to check index number of an app in the arrayList is valid
+      * @param index int representing the potential index in the arrayList
+     * @return true if the number is a valid index number, false otherwise
+     */
     public boolean isValidIndex(int index) {
         return (index >= 0) && (index < apps.size());
-    }*/
+    }
+
 
     //---------------------
     // Persistence methods
     //---------------------
-    // TODO UNCOMMENT THIS COMPLETED CODE block as you start working through this class
-    //---------------------
-    /*
+
     @SuppressWarnings("unchecked")
     public void load() throws Exception {
         //list of classes that you wish to include in the serialisation, separated by a comma
@@ -74,5 +359,5 @@ public class AppStoreAPI {
     public String fileName(){
         return "apps.xml";
     }
-    */
+
 }
